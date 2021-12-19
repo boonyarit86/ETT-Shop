3bc03f700c8e5398c1773602d1732b08d9165969
Boonyarit Ritdet <boonyaritza93@hotmail.com>
2021-12-19 17:56:43 +0700

    Made Search component of Header

--- src/shared/Navigations/Header.js
+++ src/shared/Navigations/Header.js
@@ -1,13 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 
 function Header() {
-
-    const [openMenuMobile, setOpenMenuMobile] = useState(false);
+  const [openMenuMobile, setOpenMenuMobile] = useState(false);
+  const [openSearch, setOpenSearch] = useState(false);
+  
+  useEffect(() => {
+    window.addEventListener("resize", () => {
+      let width = window.innerWidth;
+      if(width <= 758) {
+        setOpenSearch(false);
+      }
+    })
+    return () => window.removeEventListener("resize")
+  }, [])
 
   return (
     <Container>
-      <Content>
+      <Content className={openSearch ? "openSearchInput" : ""} >
         <Logo>
           <img src="/images/Ett-logo.png" />
         </Logo>
@@ -35,15 +45,25 @@ function Header() {
             </NavList>
           </NavListWrap>
         </Nav>
-        <SearchIcon>
+        <SearchIcon
+          onClick={(e) => setOpenSearch(!openSearch)}
+        >
           <img src="/images/search-icon.svg" alt="" />
         </SearchIcon>
         <HamburgurIcon onClick={(e) => setOpenMenuMobile(!openMenuMobile)}>
           <img src="/images/hamburgur-icon.svg" alt="" />
         </HamburgurIcon>
+        <SearchInput 
+        style={{ display: openSearch ? "flex" : "none" }}
+        >
+          <div>
+            <img src="/images/search-icon--black.svg" alt="" />
+            <input />
+          </div>
+        </SearchInput>
       </Content>
-      <MobileContent className={openMenuMobile ? "active": ""}>
-        Hi
+      <MobileContent className={openMenuMobile ? "active" : null}>
+        Hi {openMenuMobile ? "true" : "false"}
       </MobileContent>
     </Container>
   );
@@ -51,13 +71,48 @@ function Header() {
 
 export default Header;
 
+const Logo = styled.div`
+  img {
+    width: 48px;
+    height: 48px;
+  }
+`;
+
+const Nav = styled.div`
+  display: block;
+  flex-grow: 1;
+  @media (max-width: 768px) {
+    display: none;;
+  }
+`;
+
+const SearchIcon = styled.div`
+  img {
+    width: 28px;
+    height: 28px;
+  }
+  @media (max-width: 768px) {
+    display: none;
+  }
+`;
+
 const Container = styled.div`
   width: 100%;
+  height: 80px;
   background-color: #002ead;
   position: fixed;
   left: 0;
   top: 0;
   border-bottom: 1px solid rgba(0, 0, 0, 0.08);
+  .active {
+    display: block;
+    opacity: 1;
+  }
+  .openSearchInput {
+    ${Logo}, ${SearchIcon}, ${Nav} {
+      display: none;
+    }
+  }
 `;
 
 const Content = styled.div`
@@ -70,20 +125,6 @@ const Content = styled.div`
   position: relative;
 `;
 
-const Logo = styled.div`
-  img {
-    width: 48px;
-    height: 48px;
-  }
-`;
-
-const Nav = styled.div`
-  display: block;
-  flex-grow: 1;
-  @media(max-width: 768px) {
-        display: none;
-    }
-`;
 
 const NavListWrap = styled.ul`
   list-style: none;
@@ -102,39 +143,59 @@ const NavList = styled.li`
   }
 `;
 
-const SearchIcon = styled.div`
-    img {
-        width: 28px;
-        height: 28px;
-    }
-    @media(max-width: 768px) {
-        display: none;
-    }
-`;
-
 const HamburgurIcon = styled.div`
-    display: none;
-    img {
-        width: 28px;
-        height: 28px;
-    }
+  display: none;
+  img {
+    width: 28px;
+    height: 28px;
+  }
 
-    @media(max-width: 768px) {
-        display: block;
-        margin-left: auto;
-    }
+  @media (max-width: 768px) {
+    display: block;
+    margin-left: auto;
+  }
 `;
 
 const MobileContent = styled.div`
-    position: absolute;
-    top: 80px;
-    width: 100%;
-    height: 90%;
-    background: #000;
-    opacity: 0.8;
+  position: absolute;
+  top: 73px;
+  width: 100%;
+  height: 70vh;
+  background: #000;
+  color: #fff;
+  display: none;
+`;
 
-    &:active {
-        display: block;
-        opacity: 1;
+const SearchInput = styled.div`
+  /* height: 72px; */
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+
+  div {
+    width: 60%;
+    height: 35px;
+    background: #fff;
+    display: flex;
+    align-items: center;
+    border-radius: 5px;
+
+    input {
+      border-radius: 5px;
+      border: none;
+      background: transparent;
+      width: 90%;
+      height: 90%;
+      margin-left: 10px;
+      outline: none;
     }
+  
+    img {
+      width: 28px;
+      height: 28px;
+      margin-left: 5px;
+    }
+  }
+
 `;
