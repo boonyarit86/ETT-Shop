acd33a6391fc3385c49c7e81369e104d849f1909
Boonyarit Ritdet <boonyaritza93@hotmail.com>
2022-03-19 18:24:57 +0700

    Re-responsive Header section

--- src/shared/UIElements/Backdrop.js
+++ src/shared/UIElements/Backdrop.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Backdrop.scss";
 
 function Backdrop(props) {
-  return <div className="backdrop" onClick={props.onClick} />;
+  return <div className="backdrop" onClick={props.onClick} style={{zIndex: props.mobile && 1000}} />;
 }
 
 export default Backdrop;
