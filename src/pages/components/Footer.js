860ffb2934ffccf1f3563f863e4af540ba1f9c8c
Boonyarit Ritdet <boonyaritza93@hotmail.com>
2022-03-20 21:06:10 +0700

    Redesign Footer section

--- src/pages/components/Footer.js
+++ src/pages/components/Footer.js
@@ -1,168 +1,106 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import styled from "styled-components";
+import "./Footer.scss";
 
 function Footer() {
   return (
-    <Container>
-      <Content>
-        <Help>
-          <p>ศูนย์ให้ความช่วยเหลือ</p>
-          <List>
-            <li><Link to="/payment">การชำระเงิน</Link></li>
-            <li>สินค้าจากต่างประเทศ</li>
-          </List>
-        </Help>
-        <About>
-          <p>เกี่ยวกับ ETT</p>
-          <List>
-            <li>
-              <Link to="/about">ประวัติ ETT</Link>
-            </li>
-            <li>บทความ</li>
-            <li><Link to="/partner">ตัวแทนจำหน่าย</Link></li>
-            <li>ดาวน์โหลด</li>
-          </List>
-        </About>
-        <Contact>
-          <p><Link to="/contact" >ข้อมูลติดต่อ</Link></p>
-          <List>
-            <li>1112/96-98 ถ.สุขุมวิท แขวงพระโขนง เขตคลองเตย กรุงเทพ 10110</li>
-            <li>
-              <p>โทร:</p>02-712-1120
-            </li>
-            <li>
-              <p>แฟกซ์:</p>02-391-7216
-            </li>
-            <li>
-              <SocialLink>
-                <Icon href="https://m.facebook.com/ett.co.th" ><img src="/images/facebook-icon--white.png" alt="" /></Icon>
-                <Icon href="#" ><img src="/images/line-icon--white.png" alt="" /></Icon>
-                <Icon href="#" ><img src="/images/email-icon--white.png" alt="" /></Icon>
-              </SocialLink>
-            </li>
-          </List>
-        </Contact>
-        <div className="hh2">
-          {/* <hr /> */}
-          <p>
+    <div className="footer">
+      <div className="row">
+        <div className="footer__container">
+          <div className="footer__item">
+            <p className="footer__menu--font-white">ศูนย์ให้ความช่วยเหลือ</p>
+            <ul className="footer__menu-list">
+              <li className="footer__menu-item">
+                <Link className="footer__menu-link" to="/payment">
+                  การชำระเงิน
+                </Link>
+              </li>
+              <li className="footer__menu-item">สินค้าจากต่างประเทศ</li>
+            </ul>
+          </div>
+
+          <div className="footer__item">
+            <p className="footer__menu--font-white">เกี่ยวกับ ETT</p>
+            <ul className="footer__menu-list">
+              <li className="footer__menu-item">
+                <Link className="footer__menu-link" to="/about">
+                  ประวัติ ETT
+                </Link>
+              </li>
+              <li className="footer__menu-item">บทความ</li>
+              <li className="footer__menu-item">
+                <Link className="footer__menu-link" to="/partner">
+                  ตัวแทนจำหน่าย
+                </Link>
+              </li>
+              <li className="footer__menu-item">ดาวน์โหลด</li>
+            </ul>
+          </div>
+
+          <div className="footer__item">
+            <Link
+              className="footer__menu-link footer__menu--font-white"
+              to="/contact"
+              style={{padding: 0}}
+            >
+              ข้อมูลติดต่อ
+            </Link>
+            <ul className="footer__menu-list">
+              <li className="footer__menu-item">
+                1112/96-98 ถ.สุขุมวิท แขวงพระโขนง เขตคลองเตย กรุงเทพ 10110
+              </li>
+              <li className="footer__menu-item">
+                <p className="footer__menu--font-white">โทร:</p>02-712-1120
+              </li>
+              <li className="footer__menu-item">
+                <p className="footer__menu--font-white">แฟกซ์:</p>02-391-7216
+              </li>
+              <li className="footer__menu-item">
+                <div className="footer__social-link">
+                  <a
+                    className="footer__social-icon-box"
+                    href="https://m.facebook.com/ett.co.th"
+                  >
+                    <img
+                      className="footer__social-icon"
+                      src="/images/facebook-icon--white.png"
+                      alt="facebook icon"
+                    />
+                  </a>
+                  <a className="footer__social-icon-box" href="">
+                    <img
+                      className="footer__social-icon"
+                      src="/images/line-icon--white.png"
+                      alt="line icon"
+                    />
+                  </a>
+                  <a
+                    className="footer__social-icon-box"
+                    href="mailto: sale@etteam.com"
+                  >
+                    <img
+                      className="footer__social-icon"
+                      src="/images/email-icon--white.png"
+                      alt="email icon"
+                    />
+                  </a>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="footer__copyright">
+          <p className="footer__copyright-text">
             Copyright © 2000 - 2021 by ETT Co.,Ltd, Powered by{" "}
-            <a href="suntechnet.com">suntechnet.com</a>
+            <a className="footer__copyright-link" href="suntechnet.com">
+              suntechnet.com
+            </a>
           </p>
         </div>
-      </Content>
-    </Container>
+      </div>
+    </div>
   );
 }
 
 export default Footer;
-
-const Container = styled.div`
-  background-color: #1a1c20;
-  padding: 20px 10px;
-  width: 100%;
-`;
-
-const Content = styled.div`
-  display: flex;
-  margin: 0 auto;
-  max-width: 1128px;
-  position: relative;
-  padding-bottom: 10px;
-  @media (max-width: 500px) {
-    flex-direction: column;
-  }
-
-  .hh2 {
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    /* margin-top: 10px; */
-    p {
-      a {
-        color: #eee;
-        font-size: 12px;
-        transition: 0.3s;
-        &:hover {
-          color: #ffc709;
-        }
-      }
-    }
-    padding-left: 25px;
-    color: #eee;
-    font-size: 12px;
-  }
-`;
-
-const List = styled.ul`
-  margin-top: 10px;
-  list-style: none;
-  li {
-    color: #8b8d94;
-    padding: 5px 0;
-    max-width: 180px;
-
-    p {
-      color: #eee;
-      line-height: 2;
-    }
-  }
-
-  li {
-    a {
-      color: #8b8d94;
-      padding: 5px 0;
-      max-width: 180px;
-
-      &:hover {
-        color: #ffc709;
-      }
-    }
-  }
-`;
-
-const Help = styled.div`
-  color: #eee;
-  padding: 0 25px;
-  flex: 1;
-  margin-bottom: 10px;
-`;
-
-const About = styled(Help)``;
-
-const Contact = styled(Help)`
-  p {
-    a {
-      color: #eee;
-      &:hover {
-       color: #ffc709;
-     }
-    }
-  }
-`;
-
-const SocialLink = styled.div`
-  margin-top: 10px;
-  display: flex;
-  align-items: center;
-`;
-
-const Icon = styled.a`
-  width: 38px;
-  height: 38px;
-  background-color: #333;
-  border-radius: 50%;
-  margin-right: 5px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  &:hover {
-    background-color: #002ead;
-  }
-
-  img {
-    width: 24px;
-    height: 24px;
-  }
-`;
