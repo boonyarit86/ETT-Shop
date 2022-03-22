a1ca4725ce3397a5417a39a46bb90efe4ac9ecee
Boonyarit Ritdet <boonyaritza93@hotmail.com>
2022-03-22 21:24:19 +0700

    Create Search prompt on Header section

--- src/shared/Navigations/Header.js
+++ src/shared/Navigations/Header.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 import Backdrop from "../UIElements/Backdrop";
+import { products } from "../../Api";
 import "./Header.scss";
 
 const HEADER__ITEM_LINK__ACTIVE = "header__item-link--active";
@@ -8,49 +9,51 @@ const HEADER__ITEM_LINK__ACTIVE = "header__item-link--active";
 function Header() {
   const [openMenuMobile, setOpenMenuMobile] = useState(false);
   const [openSearch, setOpenSearch] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
+  const [suggestions, setSuggestions] = useState(products);
   const location = useLocation();
 
   useEffect(() => {
     let pathname = location.pathname;
     let header__itemAll = document.querySelectorAll(".header__item-link");
 
-    header__itemAll.forEach(ele => {
-      if(isElementActive(ele)) {
+    header__itemAll.forEach((ele) => {
+      if (isElementActive(ele)) {
         removeActiveElement(ele);
       }
-    })
+    });
 
-    if(pathname === "/") {
-      setActiveElement(header__itemAll[0])
+    if (pathname === "/") {
+      setActiveElement(header__itemAll[0]);
     }
 
-    if(pathname === "/product") {
-      setActiveElement(header__itemAll[1])
+    if (pathname === "/product") {
+      setActiveElement(header__itemAll[1]);
     }
 
-    if(pathname === "/contact") {
-      setActiveElement(header__itemAll[3])
+    if (pathname === "/contact") {
+      setActiveElement(header__itemAll[3]);
     }
 
-    function setActiveElement (ele) {
+    function setActiveElement(ele) {
       ele.classList.add(HEADER__ITEM_LINK__ACTIVE);
     }
 
-    function isElementActive (ele) {
+    function isElementActive(ele) {
       return ele.getAttribute("class").split(" ")[1] !== undefined;
     }
 
-    function removeActiveElement (ele) {
+    function removeActiveElement(ele) {
       ele.classList.remove(HEADER__ITEM_LINK__ACTIVE);
     }
 
     return;
-  }, [location.pathname])
+  }, [location.pathname]);
 
   const onClickSearchInput = () => {
     let header_searchEl = document.querySelector(".header__search-input-box");
-    
-    if(!openSearch) {
+
+    if (!openSearch) {
       header_searchEl.classList.remove("header__search-input-box--close");
       header_searchEl.classList.add("header__search-input-box--open");
     } else {
@@ -58,6 +61,12 @@ function Header() {
       header_searchEl.classList.add("header__search-input-box--close");
     }
     setOpenSearch((prev) => !prev);
+    onClickLink();
+
+    function onClickLink() {
+      setSearchValue("");
+      setSuggestions(products);
+    }
   };
 
   const onClickMenuMobile = () => {
@@ -70,6 +79,21 @@ function Header() {
       mobile__menuEl.classList.add("mobile-menu--close");
     }
     setOpenMenuMobile((prev) => !prev);
+  };
+
+  const onTextChanged = (e) => {
+    const value = e.target.value;
+    let suggestions = [];
+    if (value.length > 0) {
+      const regex = new RegExp(`^${value}`, "i");
+      suggestions = products.sort().filter((res) => regex.test(res.title));
+    }
+
+    if (value.length === 0) {
+      suggestions = products;
+    }
+    setSearchValue(value);
+    setSuggestions(suggestions);
   };
 
   return (
@@ -89,18 +113,27 @@ function Header() {
                 <div className="header__menu">
                   <ul className="header__list">
                     <li className="header__item">
-                      <Link className="header__item-link" to="/">หน้าแรก</Link>
+                      <Link className="header__item-link" to="/">
+                        หน้าแรก
+                      </Link>
                     </li>
                     <li className="header__item">
-                      <Link className="header__item-link" to="/product">สินค้า</Link>
+                      <Link className="header__item-link" to="/product">
+                        สินค้า
+                      </Link>
                     </li>
                     <li className="header__item">
-                      <a className="header__item-link" href="http://www.ett.co.th/article/article.html">
+                      <a
+                        className="header__item-link"
+                        href="http://www.ett.co.th/article/article.html"
+                      >
                         บทความ
                       </a>
                     </li>
                     <li className="header__item">
-                      <Link className="header__item-link" to="/contact">ติดต่อ</Link>
+                      <Link className="header__item-link" to="/contact">
+                        ติดต่อ
+                      </Link>
                     </li>
                   </ul>
                 </div>
@@ -111,22 +144,38 @@ function Header() {
                 ></ion-icon>
               </React.Fragment>
             )}
-            
-              <div className="header__search-input-box header__search-input-box--close">
-                <div className="header__search-input-item ">
-                  <ion-icon
-                    class="header__search-input-icon"
-                    name="arrow-back-outline"
-                    onClick={onClickSearchInput}
-                  ></ion-icon>
-                  <input
-                    type="text"
-                    name="search"
-                    className="header__search-input"
-                  />
-                </div>
+
+            <div className="header__search-input-box header__search-input-box--close">
+              <div className="header__search-input-item ">
+                <ion-icon
+                  class="header__search-input-icon"
+                  name="arrow-back-outline"
+                  onClick={onClickSearchInput}
+                ></ion-icon>
+                <input
+                  type="text"
+                  name="search"
+                  className="header__search-input"
+                  onChange={onTextChanged}
+                  value={searchValue}
+                  autoComplete="off"
+                  placeholder="ค้นหารายการสินค้า"
+                />
+                <ul className="header__search-input-result">
+                  {suggestions.length === 0 ? (
+                    <li>ไม่พบรายการสินค้า</li>
+                  ) : (
+                    suggestions.map((item, index) => (
+                      <li key={index}>
+                        <Link to="/product/detail" onClick={onClickSearchInput}>
+                          {item.title}
+                        </Link>
+                      </li>
+                    ))
+                  )}
+                </ul>
               </div>
-            
+            </div>
           </div>
         </div>
       </div>
@@ -139,7 +188,11 @@ function Header() {
             name="close-outline"
             onClick={onClickMenuMobile}
           ></ion-icon>
-          <Link to="/" className="mobile-menu__logo-box" onClick={onClickMenuMobile}>
+          <Link
+            to="/"
+            className="mobile-menu__logo-box"
+            onClick={onClickMenuMobile}
+          >
             <img
               src="/images/ett-logo.png"
               className="mobile-menu__logo-img"
@@ -149,7 +202,11 @@ function Header() {
         </div>
         <ul className="mobile-menu__list">
           <li className="mobile-menu__item">
-            <Link to="/" className="mobile-menu__link" onClick={onClickMenuMobile}>
+            <Link
+              to="/"
+              className="mobile-menu__link"
+              onClick={onClickMenuMobile}
+            >
               <ion-icon
                 class="mobile-menu__link-icon"
                 name="home-outline"
@@ -158,7 +215,11 @@ function Header() {
             </Link>
           </li>
           <li className="mobile-menu__item">
-            <Link to="/product" className="mobile-menu__link" onClick={onClickMenuMobile}>
+            <Link
+              to="/product"
+              className="mobile-menu__link"
+              onClick={onClickMenuMobile}
+            >
               <ion-icon
                 class="mobile-menu__link-icon"
                 name="bag-handle-outline"
@@ -167,7 +228,11 @@ function Header() {
             </Link>
           </li>
           <li className="mobile-menu__item">
-            <Link to="#" className="mobile-menu__link" onClick={onClickMenuMobile}>
+            <Link
+              to="#"
+              className="mobile-menu__link"
+              onClick={onClickMenuMobile}
+            >
               <ion-icon
                 class="mobile-menu__link-icon"
                 name="reader-outline"
@@ -176,7 +241,11 @@ function Header() {
             </Link>
           </li>
           <li className="mobile-menu__item">
-            <Link to="/contact" className="mobile-menu__link" onClick={onClickMenuMobile}>
+            <Link
+              to="/contact"
+              className="mobile-menu__link"
+              onClick={onClickMenuMobile}
+            >
               <ion-icon
                 class="mobile-menu__link-icon"
                 name="call-outline"
